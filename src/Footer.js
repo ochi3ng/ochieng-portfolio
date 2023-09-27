6a58ddcc8b26b92ea5519997284eb84cb17210a8
ochi3ng <johnochi3ng@gmail.com>
2023-09-27 07:11:15 +0300

    making correction

---
+++ src/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./Footer.css"
+function Footer() {
+  return (
+    <div className='footerdetails'>
+
+     <div>
+              <div>Contact: +254727291397</div>
+              <div>Email: johnochi3ng@gmail</div>
+     </div>
+     <div>
+              <div className='more'>About Me</div>
+              <div className='massege'>A visual display of some of my best works. Still a work in progress.
+                 Feel free to reach out to me for constructive criticism and opportunities!</div>
+     </div>
+    </div>
+  )
+}
+
+export default Footer
--- src/Home.js
+++ src/Home.js
@@ -2,16 +2,18 @@ import React from 'react'
 import './Home.css'
 import Navbar from './Navbar'
 import Portfolio from './Portfolio'
+import Footer from './Footer'
 function Home() {
   return (
    <div>
           <div className='homepage'>
               <Navbar />
               <h3>HI, MY NAME IS JOHN OCHIENG AND</h3>
-              <h2>I`M A FRONTEND DEVELOPER</h2>
+              <div className='homedetails'>I`M A FRONTEND DEVELOPER</div>
               
           </div>
           <Portfolio />
+          <Footer/>
    </div>
   )
 }
